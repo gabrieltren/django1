25c3c6673b0e9e016126b49dbb2c9178a91b647d
Gabrieltren <gabriel.silveira1207@gmail.com>
2022-08-01 19:53:21 -0300

    Bootstrap 1

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,6 @@ from django.db import models
 
 class Post(models.Model):
     titulo = models.CharField(max_length=100)
-    texto = models.TextField()
+    texto = models.TextField(null=True, blank=True)
+    data = models.DateField(null=True, blank=True)
+    imagem = models.ImageField(null=True, blank=True, upload_to="blog")
