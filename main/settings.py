11837b2d59662326f726401d924a39d04bfce129
Gabrieltren <gabriel.silveira1207@gmail.com>
2022-08-29 17:27:20 -0300

    heroku

--- main/settings.py
+++ main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f2&94l@cw^qyq@n=zvr(lf0v7n(x_76qk1%4xf*35j8d5-ue6$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -127,6 +127,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 django_on_heroku.settings(locals())
